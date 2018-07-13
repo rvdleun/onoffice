ce30379aea85053f7c2291564166fcf9cf8e448f
Ruben van der Leun <ruben@leunix.nl>
2018-07-13 08:03:11 +0200

    Store sky

--- src/electron/components/environment.component.js
+++ src/electron/components/environment.component.js
@@ -1,14 +1,31 @@
 'use strict';
 
+const storage = require('electron-json-storage');
+
 let sky = require('./environment/default-image');
 
 module.exports.init = function(global) {
     global.getSky = function(cb) {
-        cb(sky);
+        storage.get('sky', (error, data) => {
+            if (error) {
+                cb(sky);
+                return;
+            }
+
+            if (data.sky) {
+                sky = data.sky;
+            }
+
+            cb(sky);
+        });
     };
 
     global.setSky = function(newSky) {
         sky = newSky;
+
+        storage.set('sky', { sky }, (error) => {
+            console.log(error);
+        });
     }
 }
 
