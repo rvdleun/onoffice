0a09713dd0364b5a48992d867f09175cceabce51
Ruben van der Leun <ruben@leunix.nl>
2018-10-13 17:52:40 +0200

    Added install-wine.sh

---
+++ build/install-wine.sh
@@ -0,0 +1,6 @@
+sudo dpkg --add-architecture i386
+wget -nc https://dl.winehq.org/wine-builds/Release.key
+sudo apt-key add Release.key
+sudo apt-add-repository https://dl.winehq.org/wine-builds/ubuntu/
+sudo apt-get update
+sudo apt-get -y install --install-recommends winehq-stable
