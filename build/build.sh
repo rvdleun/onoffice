5339a98e2ca0012fb5dae9e23f89950c8e46867f
Ruben van der Leun <ruben@leunix.nl>
2018-10-13 14:39:40 +0200

    Added build directory

---
+++ build/build.sh
@@ -0,0 +1,15 @@
+# If virtual-office doesn't exist yet
+cd ..
+npm install
+npm run build-electron
+
+cd build
+if [ ! -d "node_modules" ]; then
+  ln -s ../node_modules
+fi
+
+if [ ! -d "dist" ]; then
+  ln -s ../dist
+fi
+
+npm run package-mac
