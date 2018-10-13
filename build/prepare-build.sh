69fc3464a924d3014dc446bf7633940359160db1
Ruben van der Leun <ruben@leunix.nl>
2018-10-13 16:15:09 +0200

    More work on build scripts

--- build/prepare-build.sh
+++ build/prepare-build.sh
@@ -1,16 +1,13 @@
-# If virtual-office doesn't exist yet
 cd ..
 npm install
 npm rebuild node-sass --force
 npm run build-electron
 
 cd build
-if [ ! -d "node_modules" ]; then
-  ln -s ../node_modules
-fi
-
 if [ ! -d "dist" ]; then
   ln -s ../dist
 fi
 
-npm run package-mac
+npm install
+
+echo 'Now run npm run package-linux / package-mac / package-windows to create a build'
