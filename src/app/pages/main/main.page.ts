0ef2fddbfaa2f41941fd07d079333d99c9a26302
Ruben van der Leun <ruben@leunix.nl>
2018-10-16 17:55:23 +0200

    Added animations

--- src/app/pages/main/main.page.ts
+++ src/app/pages/main/main.page.ts
@@ -4,21 +4,43 @@ import {SourceSelection} from './settings-screen/source-toggle/source-toggle.com
 import {SocketService} from '../../shared/socket.service';
 import {StreamService} from '../../shared/stream.service';
 
+type ScreenId = 'settings' | 'streaming';
+
 @Component({
     selector: 'app-page-main',
     styleUrls: ['./main.page.css'],
     templateUrl: './main.page.html'
 })
 export class MainPageComponent {
-    public activeScreen: 'settings' | 'streaming' = 'settings';
+    public activeScreen: ScreenId = null;
+    public active: boolean = true;
+    public backgroundClass: 'blue' | 'no-transition' = 'no-transition';
 
     constructor(streamService: StreamService) {
         streamService.statusSubject.subscribe((status) => {
             if (status.current === 'inactive') {
-                this.activeScreen = 'settings';
+                this.transitionTo('settings');
             } else if (this.activeScreen === 'settings') {
-                this.activeScreen = 'streaming';
+                this.transitionTo('streaming');
             }
         });
+    }
+
+    private transitionTo(screen: ScreenId) {
+        if (this.backgroundClass === 'no-transition') {
+            this.activeScreen = screen;
+            this.active = true;
+            window.setTimeout(() => this.backgroundClass = null);
+            return;
+        }
+        this.active = false;
+
+        window.setTimeout(() => {
+            this.backgroundClass = screen === 'streaming' ? 'blue' : null;
+            this.activeScreen = screen;
+            window.setTimeout(() => {
+                this.active = true;
+            }, 750);
+        }, 1250);
     }
 }
