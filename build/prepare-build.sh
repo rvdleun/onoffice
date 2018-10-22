f0ec80118235d04c7c1959a4909babecff4f4321
Ruben van der Leun <ruben@leunix.nl>
2018-10-22 08:07:00 +0200

    Made changes to prepare-build.sh to fix broken build

--- build/prepare-build.sh
+++ build/prepare-build.sh
@@ -4,14 +4,21 @@ npm install
 npm rebuild node-sass --force
 npm run build-electron
 
+cd dist
+rm client
+cd ..
+
 cd build
 if [ -d "dist" ]; then
   rm -rf dist
 fi
 
 cp -r ../dist .
-npm install
+cd dist
+cp -r ../../client .
+cd ..
 
+npm install
 node ./minify-client.js
 
 echo 'Now run npm run package-linux / package-mac / package-windows to create a build'
