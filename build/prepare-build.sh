562b45edb0a4f613e7b7ba4a27272b7d71cab18f
Ruben van der Leun <ruben@leunix.nl>
2019-12-13 17:33:01 +0100

    feature(prepare-build): Added install in build directory and comments

--- build/prepare-build.sh
+++ build/prepare-build.sh
@@ -1,10 +1,15 @@
 #!/usr/bin/env bash
+# Build application
 cd ..
 npm install
 npm rebuild node-sass --force
 npm run build
 
+# Setup build directory, install necessary modules
 cd build
+npm install
+
+# Remove dist directory
 if [ -d "dist" ]; then
   rm -rf dist
 fi
