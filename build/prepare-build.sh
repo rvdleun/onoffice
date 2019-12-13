46379b98fbad82cbfd410bddb858f1c50f141e0c
Ruben van der Leun <ruben@leunix.nl>
2019-12-13 15:03:35 +0100

    chore(build): Cleaned up commands, setup build for vue.js

--- build/prepare-build.sh
+++ build/prepare-build.sh
@@ -2,23 +2,12 @@
 cd ..
 npm install
 npm rebuild node-sass --force
-npm run build-electron
-
-cd dist
-rm client
-cd ..
+npm run build
 
 cd build
 if [ -d "dist" ]; then
   rm -rf dist
 fi
-
 cp -r ../dist .
-cd dist
-cp -r ../../client .
-cd ..
-
-npm install
-node ./minify-client.js
 
 echo 'Now run npm run package-linux / package-mac / package-windows to create a build'
