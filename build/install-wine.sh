69c2908b597d8692336cf2e407e7c4a797ac3a56
Ruben van der Leun <ruben@leunix.nl>
2020-02-15 22:27:09 +0100

    chore(wine): Changed repostiory

--- build/install-wine.sh
+++ build/install-wine.sh
@@ -2,6 +2,6 @@
 sudo dpkg --add-architecture i386
 wget -nc https://dl.winehq.org/wine-builds/winehq.key
 sudo apt-key add winehq.key
-sudo apt-add-repository 'deb https://dl.winehq.org/wine-builds/ubuntu/ bionic main'
+sudo apt-add-repository 'deb https://dl.winehq.org/wine-builds/ubuntu/ eoan main'
 sudo apt update
 sudo apt install --install-recommends winehq-stable
