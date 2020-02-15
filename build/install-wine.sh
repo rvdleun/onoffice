2cb53a3a880a1e46a56647a56fb3d5d63243efd5
Ruben van der Leun <ruben@leunix.nl>
2020-02-15 22:37:47 +0100

    chore(wine): Automatically continue during install

--- build/install-wine.sh
+++ build/install-wine.sh
@@ -4,4 +4,4 @@ wget -nc https://dl.winehq.org/wine-builds/winehq.key
 sudo apt-key add winehq.key
 sudo apt-add-repository 'deb https://dl.winehq.org/wine-builds/ubuntu/ xenial main'
 sudo apt update
-sudo apt install --install-recommends winehq-stable
+sudo apt-get -y install --install-recommends winehq-stable
